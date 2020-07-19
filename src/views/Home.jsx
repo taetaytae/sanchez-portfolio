3d04b619e92f19f761292f90f8e2af81ed00f2e1
Santos O. Martínez Braña <santos.martinez1@upr.edu>
2020-07-18 22:37:15 -0400

    Added 3d effect to logo. Created hover animations. Changed scrollbar visuals.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -12,10 +12,11 @@ export default class Home extends React.Component {
           frameborder="no"
           allow="autoplay"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/537965922&color=%23f2545b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          title='Akiko'
         ></iframe>
        
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          <span>L</span>orem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
           recusandae magni, ea earum minus in quas et, rem debitis voluptatem
           nostrum blanditiis ipsum architecto, aliquid praesentium. Fuga nostrum
           praesentium voluptates facilis molestias velit ex, voluptas
