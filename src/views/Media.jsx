80dc8f51a83d7867169f0a175df2fca42e64dd21
Mario Sanchez <mario.a.sanchez199902@gmail.com>
2020-07-19 04:22:58 +0100

    Implement media stage 2nd

--- src/views/Media.jsx
+++ src/views/Media.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import '../css/Media.css';
+import React from "react";
+import "../css/Media.css";
+import Card from '../components/Card';
 
-export default class Media extends React.Component{
-
-
-render(){
-    return(
-        <div>Media</div>
+export default class Media extends React.Component {
+  render() {
+    return (
+      <div className="mediaContainer">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     );
-}
-
+  }
 }
