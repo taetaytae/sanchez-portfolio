c8107a5c8ff6a27df2472a22dad42259526d97a0
Mario Sanchez <mario.a.sanchez199902@gmail.com>
2020-07-20 01:25:48 +0100

    Implemented contact information page

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import '../css/Contact.css';
+import React from "react";
+import "../css/Contact.css";
+import twitter from "../assets/twitter.svg";
+import instagram from "../assets/instagram.svg";
+import youtube from "../assets/youtube.svg";
+import apple from "../assets/apple.svg";
 
-export default class Contact extends React.Component{
+export default class Contact extends React.Component {
+  render() {
+    return (
+      <div className="contactContainer">
+        <h1>Send me an email</h1>
+        <a className="email" href="mailto:mario.a.sanchez199902@gmail.com">
+          mario.a.sanchez199902
+          <span>@</span>
+          gmail.com
+        </a>
 
+        <h1>Socials:</h1>
 
-render(){
-    return(
-        <div className='contactContainer'>Contact</div>
+        <div className="socialLinks">
+          <a className="twitterLogo " href="https://twitter.com/">
+            <img src={twitter} alt='twiiter' />
+          </a>
+          <a className="instagramLogo" href="https://www.instagram.com/">
+            <img src={instagram} alt='instagram' />
+          </a>
+          <a className="youtubeLogo" href="https://www.youtube.com/">
+            <img src={youtube} alt='youtube' />
+          </a>
+          <a
+            className="appleLogo"
+            href="https://www.apple.com/apple-music/"
+          >
+            <img src={apple} alt='apple' />
+          </a>
+        </div>
+      </div>
     );
-}
-
+  }
 }
