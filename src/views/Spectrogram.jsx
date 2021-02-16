02a107d85ad4cda3ebb6564946fd6d6eae9238b8
Mario Sanchez <mario.a.sanchez199902@gmail.com>
2021-02-15 22:20:27 -0500

    Small text edit

--- src/views/Spectrogram.jsx
+++ src/views/Spectrogram.jsx
@@ -10,7 +10,7 @@ export default class Spectrogram extends React.Component {
                     <h2>Spectrogram (2019)</h2>
 
                     <p>The objective of this project was to build a spectrogram that 
-                        allowed the user to see an analysis of a loaded in .wav file along 
+                        allowed the user to see a real-time analysis of a loaded in .wav file along 
                         with giving the user the ability to apply some form of filtering to the audio 
                         file so they can see how this affects the spectrogram analysis. 
                         
